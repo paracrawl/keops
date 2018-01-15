e7d22c5152a9321fca903b8184bec9ea3127cb28
Jorge Ferrández-Tordera <jferrandez@prompsit.com>
2018-01-15 08:51:25 +0100

    First KEOPS commit with PHP / Bootstrap

--- resources/config.php
+++ resources/config.php
@@ -2,18 +2,13 @@
 $config = array(
     "db" => array(
         "db1" => array(
-            "dbname" => "database1",
-            "username" => "dbUser",
-            "password" => "pa$$",
-            "host" => "localhost"
-        ),
-        "db2" => array(
-            "dbname" => "database2",
-            "username" => "dbUser",
-            "password" => "pa$$",
-            "host" => "localhost"
+            "dbname" => "keopsdb",
+            "username" => "keopsdb",
+            "password" => ".keopsdb.",
+            "host" => "localhost",
+            "port" => "5432"
         )
-    ),
+    )/*,
     "urls" => array(
         "baseUrl" => "http://example.com"
     ),
@@ -23,9 +18,8 @@ $config = array(
             "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
             "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
         )
-    )
+    )*/
 );
 
 defined("TEMPLATES_PATH")
     or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
-?>
