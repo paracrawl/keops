4d76630a92a3be32759eb78ff4a63b79b580f5ef
mbanon <mbanon@prompsit.com>
2018-01-17 13:11:06 +0100

    PG files

---
+++ dao/user_dao.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'db/pg_conn.php';
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+  $conn = postgresql_connect('keopsdb', '.keopsdb.', 'localhost', '5432', 'keopsdb' );
+  
+  function get_users(){
+    try {
+      
+    } catch (Exception $ex) {
+
+    }
+  }
+ ?>
