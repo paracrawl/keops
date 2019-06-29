e351b088a45183854af1317be025a65266c7cc9b
motagirl2 <mbanon@prompsit.com>
2019-06-29 14:18:08 +0200

    Docker fix

--- configure-keops.sh
+++ configure-keops.sh
@@ -4,8 +4,8 @@ echo "STARTING CONFIG..."
 
 cp /opt/keops/keops.conf /etc/nginx/sites-available/keops.conf && cd /etc/nginx/sites-enabled && rm default && sudo ln -s /etc/nginx/sites-available/keops.conf && cd /opt/keops
 
-echo "extension=pdo_pgsql.so" >> /etc/php/7.2/fpm/php.ini
-echo "extension=pgsql.so" >> /etc/php/7.2/fpm/php.ini
+echo "extension=pdo_pgsql" >> /etc/php/7.2/fpm/php.ini
+echo "extension=pgsql" >> /etc/php/7.2/fpm/php.ini
 
 service postgresql start && sudo -u postgres createdb keopsdb && sudo -u postgres psql keopsdb < keopsdb_init.sql && service postgresql stop
 
