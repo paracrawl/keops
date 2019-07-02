23bf686f4f9085fa080f09207d31698af7032754
Sergio Juan <sjarmero@prompsit.com>
2019-07-02 12:13:55 +0200

    Sentences are no longer discarded when uploaded to Keops (which fixes issue #29)

--- corpora/corpus_upload.php
+++ corpora/corpus_upload.php
@@ -35,7 +35,10 @@ try {
         while (!feof($handle)) // Loop 'til end of file.
         {
             $buffer = fgets($handle); // Read a line.
-            $data = str_getcsv($buffer, "\t");
+            $data = explode("\t", $buffer);
+
+            if (count($data) > 2) { $data = array_slice($data, 0, 2); }
+
             if (!empty(trim($buffer)) && count($data) == 2 && strlen($data[0]) <= 5000 && strlen($data[1]) <= 5000) {
               $values[] = $data;// save values
             }
