4d96fb225a93f59f30ca330f305ebd4957de433d
Sergio Juan <sjarmero@prompsit.com>
2019-08-30 10:46:24 +0200

    Search fix

--- dao/sentence_task_dao.php
+++ dao/sentence_task_dao.php
@@ -295,8 +295,8 @@ class sentence_task_dao {
         . "left join sentences as s2 on (sp.id_2 = s2.id) "
         . "where st.task_id = :taskid and st.sentence_id not in (select id_2 from sentences_pairing) "
         . (($label != "ALL") ? "and st.evaluation = :label " : "")
-        . (($search_term != "") ? "and (s.source_text_vector @@ to_tsquery(:searchterm) " : "")
-        . (($search_term != "") ? "or s2.source_text_vector @@ to_tsquery(:searchterm2))" : " ")
+        . (($search_term != "") ? "and (s.source_text @@ to_tsquery(:searchterm) " : "")
+        . (($search_term != "") ? "or s2.source_text @@ to_tsquery(:searchterm2))" : " ")
         . "order by st.id asc limit 1 offset :offset;");
       
       $query->bindParam(':taskid', $task_id);
@@ -429,13 +429,20 @@ class sentence_task_dao {
     try {
       $task_progress_dto = new task_progress_dto();
       $query = $this->conn->prepare("
-        select count(case when st.id <= :sentenceid then 1 end) as current, count(distinct st.id) as total, count(case when evaluation<>'P' then 1 end) as completed from sentences_tasks as st left join sentences as s on (s.id = st.sentence_id) "
-        . "join sentences_pairing as sp on (st.sentence_id = sp.id_1) "
-        . "join sentences as s2 on (sp.id_2 = s2.id) "
-        . "where task_id = :taskid " 
-        . (($label != "ALL") ? "and evaluation = :label " : "")
-        . (($search_term != "") ? "and (s.source_text_vector @@ to_tsquery(:searchterm) " : "")
-        . (($search_term != "") ?" or s2.source_text_vector @@ to_tsquery(:searchterm2))" : ";")
+      select count(case when x.id <= :sentenceid then 1 end) as current, 
+        count(x.id) as total, count(case when evaluation<>'P' then 1 end) as completed 
+        from (
+          select st.id, evaluation
+          from sentences_tasks as st 
+          left join sentences as s on (s.id = st.sentence_id)
+          join sentences_pairing as sp on (st.sentence_id = sp.id_1)
+          join sentences as s2 on (sp.id_2 = s2.id) "
+          . "where task_id = :taskid and sentence_id not in (select id_2 from sentences_pairing) " 
+          . (($label != "ALL") ? "and evaluation = :label " : "")
+          . (($search_term != "") ? "and (s.source_text @@ to_tsquery(:searchterm) " : "")
+          . (($search_term != "") ?" or s2.source_text @@ to_tsquery(:searchterm2))" : "")
+          . " group by st.id
+        ) as x;"
       );
 
       $query->bindParam(':sentenceid', $sentence_id);
