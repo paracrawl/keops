eafada8009bf95976618a08438c81cd5cb651858
Sergio Juan <sjarmero@prompsit.com>
2019-09-23 11:38:43 +0200

    Valid is green on chart

--- js/general.js
+++ js/general.js
@@ -125,17 +125,44 @@ $(document).ready(function() {
     stateSave: true
   });
   
+  function rainbow(numOfSteps, step) {
+    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+    // Adam Cole, 2011-Sept-14
+    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+    var r, g, b;
+    var h = step / numOfSteps;
+    var i = ~~(h * 6);
+    var f = h * 6 - i;
+    var q = 1 - f;
+    switch(i % 6){
+        case 0: r = 1; g = f; b = 0; break;
+        case 1: r = q; g = 1; b = 0; break;
+        case 2: r = 0; g = 1; b = f; break;
+        case 3: r = 0; g = q; b = 1; break;
+        case 4: r = f; g = 0; b = 1; break;
+        case 5: r = 1; g = 0; b = q; break;
+    }
+
+    var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 100)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+    return (c);
+}
+
   /**
    * Completion chart for a task
    */
+  let colors = ["#21b00b"];
+  for (let i = 0; i < labels_pie_chart.length - 1; i++) {
+    colors.push(rainbow(labels_pie_chart.length, ((i > 1) ? (i + 1) : i)))
+  }
+
   if (document.getElementById("pie-chart") && labels_pie_chart && data_pie_chart) {
     var recapChart = new Chart(document.getElementById("pie-chart"), {
       type: 'pie',
       data: {
         labels: labels_pie_chart,
         datasets: [{
-          backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#3e95cd", "#8e5ea2","#3cba9f","#e2782c", "#4D5360","#20a96a"],
-          hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#50aeea", "#c184da", "#46d6b7", "#f38f48", "#616774", "#4bcc91"],
+          backgroundColor: colors,
+          hoverBackgroundColor: colors,
           data: data_pie_chart
         }]
       },
